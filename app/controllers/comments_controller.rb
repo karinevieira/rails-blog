abe0bad9468c806d4af3d5695a66afcaf8ac6cbf
Karine Vieira <kv.vieira1@gmail.com>
2022-02-15 16:41:43 -0300

    ✨ add destroy action for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,13 @@ class CommentsController < ApplicationController
     redirect_to article_path(@article), notice: 'Comentário criado com sucesso!'
   end
 
+  def destroy
+    comment = @article.comments.find(params[:id])
+
+    comment.destroy
+    redirect_to article_path(@article), notice: 'Comentário excluído com sucesso!'
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:body)
