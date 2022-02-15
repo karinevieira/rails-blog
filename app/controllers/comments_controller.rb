32ad165acdd6825fd671a3391c2c8cb7f8a8266f
Karine Vieira <kv.vieira1@gmail.com>
2022-02-15 17:07:04 -0300

    ✨ add commenter

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,7 +15,7 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:body, :commenter)
   end
 
   def set_article
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220214200832) do
+ActiveRecord::Schema.define(version: 20220215194835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20220214200832) do
     t.integer  "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "commenter"
     t.index ["article_id"], name: "index_comments_on_article_id", using: :btree
   end
 
